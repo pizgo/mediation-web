1640cad0bdf6385066f91108b20e299094c9fd9a
Kasia Półrolniczak <k.polrolniczak@gmail.com>
2022-10-03 15:23:33 +0200

    adding content from sanity to the landing page offer component

--- src/components/LandingPage/LandingPageOffer.js
+++ src/components/LandingPage/LandingPageOffer.js
@@ -2,30 +2,55 @@ import { useEffect, useState} from "react";
 import sanityClient from "../../client.js";
 
 const LandingPageOffer = () => {
-    // const [landingHeadText, setLandingHeadText] = useState(null);
-    // const [landingAboutText, setLandingAboutText] = useState(null);
-    //
-    // useEffect(() => {
-    //     sanityClient.fetch(
-    //             `*[_type == "plainText" && title == "LPHead"]{
-    //         body
-    //     }`)
-    //         .then((data) => {
-    //             setLandingHeadText(data)
-    //         })
-    //         .catch(console.error);
-    // }, []);
+    const [landingOfferText, setLandingOfferText] = useState(null);
+
+    useEffect(() => {
+        sanityClient.fetch(
+            `*[_type == "offer"]{
+                title,
+                mainImage{
+                    asset-> {
+                    _id,
+                    url
+                }},
+                alt,
+                body
+        }`)
+            .then((data) => {
+                setLandingOfferText(data)
+            })
+            .catch(console.error);
+    }, []);
 
     return (
         <>
-            {/*<div>*/}
-            {/*    {landingHeadText &&*/}
-            {/*    landingHeadText.map((headText, index) => (*/}
-            {/*        <h3>*/}
-            {/*            {headText.body}*/}
-            {/*        </h3>*/}
-            {/*    ))}*/}
-            {/*</div>*/}
+            <main>
+                <section>
+                    <h2>
+                        Jak mogę ci pomóc?
+                    </h2>
+                    <div>
+                        {landingOfferText &&
+                        landingOfferText.map((text, index) => (
+                            <article>
+                  <span key={index}>
+                      <p>{text.title}</p>
+                    <img
+                        src={text.mainImage.asset.url}
+                        alt={text.mainImage.alt}
+                    />
+                    <span>
+                      <p>
+                        {text.body}
+                      </p>
+                    </span>
+                  </span>
+
+                            </article>
+                        ))}
+                    </div>
+                </section>
+            </main>
         </>
     );
 
