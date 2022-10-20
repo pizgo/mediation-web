bc1e42e68e3ebe7a6be9c38deafe40b0bc54f297
Kasia Półrolniczak <k.polrolniczak@gmail.com>
2022-10-20 13:28:57 +0200

    adding fonts

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,29 +4,27 @@ const Navbar = () => {
 
 return (
        <>
-           <div className="container">
-               <section className="nav">
-                   <div className="nav__container">
-                       <ul className="nav__elements">
-                           <Link className="nav__link" to="/">
-                               Start
-                           </Link>
-                           <Link className="nav__link" to="/omnie">
-                               O mnie
-                           </Link>
-                           <Link className="nav__link" to="/omediacjach">
-                               O mediacjach
-                           </Link>
-                           <Link className="nav__link" to="/oferta">
-                               Oferta
-                           </Link>
-                           <Link className="nav__link" to="/kontakt">
-                               Kontakt
-                           </Link>
-                       </ul>
-                   </div>
-               </section>
-           </div>
+           <nav className="navbar navbar-expand-lg">
+               <div className="container-fluid">
+                   <ul className="nav__elements">
+                       <Link className="nav__link" to="/">
+                           Start
+                       </Link>
+                       <Link className="nav__link" to="/omnie">
+                           O mnie
+                       </Link>
+                       <Link className="nav__link" to="/omediacjach">
+                           O mediacjach
+                       </Link>
+                       <Link className="nav__link" to="/oferta">
+                           Oferta
+                       </Link>
+                       <Link className="nav__link" to="/kontakt">
+                           Kontakt
+                       </Link>
+                   </ul>
+               </div>
+           </nav>
        </>
     )
 };
