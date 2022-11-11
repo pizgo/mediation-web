87f41d2040d94b5ef0a6385ec059733b47fc7f85
Katarzyna Półrolniczak <k.polrolniczak@gmail.com>
2022-11-11 17:52:02 +0100

    finished new design for LandingPageIntro section

--- src/components/LandingPage/LandingPageIntro.js
+++ src/components/LandingPage/LandingPageIntro.js
@@ -20,18 +20,18 @@ return (
                {/*        <h3 className="intro-desc">Rozwiązywanie sporów</h3>*/}
                {/*    </div>*/}
                {/*</div>*/}
-               <div className="row pt-5">
-                   <div className="col-md-6">
+               <div className="row pt-2 pt-md-4 pt-lg-5">
+                   <div className="col-md-7 col-lg-6">
                        <div className="intro-img-container d-flex justify-content-center">
                            <img src={photoBig} alt='zdjecie'
                                 className="img-fluid img-intro "/>
                        </div>
                    </div>
-                   <div className="col-md-5 d-flex flex-column justify-content-center">
-                       <h2 className="display-4 intro-name pb-2">Izabela Kowalska</h2>
-                       <h3 className="intro-desc">Mediacje</h3>
-                       <h3 className="intro-desc">Negocjacje</h3>
-                       <h3 className="intro-desc">Rozwiązywanie sporów</h3>
+                   <div className="col-md-5 col-lg-6 pt-3 pt-md-0 d-flex flex-column justify-content-center">
+                       <h2 className="display-4 intro-name text-center text-md-left pb-1 pb-md-4">Izabela Kowalska</h2>
+                       <h3 className="intro-desc text-center text-md-left">Mediacje</h3>
+                       <h3 className="intro-desc text-center text-md-left">Negocjacje</h3>
+                       <h3 className="intro-desc text-center text-md-left">Rozwiązywanie sporów</h3>
                    </div>
                </div>
            </div>
