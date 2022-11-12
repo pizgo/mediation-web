f72fae4efbf439ec8b784ec97b8bd1ae803f4948
Kasia Półrolniczak <k.polrolniczak@gmail.com>
2022-11-12 13:31:44 +0100

    added fade in animation to landingPage

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -2,16 +2,22 @@ import LandingPageHead from "./LandingPageHead";
 import LandingPageAboutMediation from "./LandingPageAboutMediation";
 import LandingPageOffer from "./LandingPageOffer";
 import LandingPageAboutMe from "./LandingPageAboutMe";
+import {motion} from "framer-motion";
 
 const LandingPage = () => {
 
 
     return (
         <>
-            <LandingPageHead/>
-            <LandingPageAboutMediation/>
-            <LandingPageOffer/>
-            <LandingPageAboutMe/>
+            <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ease: "easeOut", duration: 1}}>
+                <LandingPageHead/>
+                <LandingPageAboutMediation/>
+                <LandingPageOffer/>
+                <LandingPageAboutMe/>
+            </motion.div>
         </>
     )
 }
--- src/components/LandingPage/LandingPageHead.js
+++ src/components/LandingPage/LandingPageHead.js
@@ -1,44 +1,26 @@
 import photoBig from '../../assets/photoBig.png';
-
+import {motion} from 'framer-motion';
 const LandingPageHead= () => {
 
 return (
        <section className="head-section">
-           <div className="container">
-               <div className="row pt-2 pt-md-4 pt-lg-5">
-                   <div className="col-md-7 col-lg-6">
-                       <div className="head-img-container d-flex justify-content-center">
-                           <img src={photoBig} alt='zdjecie'
-                                className="img-fluid img-head"/>
+               <div className="container">
+                   <div className="row pt-2 pt-md-4 pt-lg-5">
+                       <div className="col-md-7 col-lg-6">
+                           <div className="head-img-container d-flex justify-content-center">
+                               <img src={photoBig} alt='zdjecie'
+                                    className="img-fluid img-head"/>
+                           </div>
+                       </div>
+                       <div className="col-md-5 col-lg-6 pt-3 pt-md-0 d-flex flex-column justify-content-center">
+                           <h2 className="display-4 head-name text-center text-md-start pb-1 pb-md-4">Izabela Kowalska</h2>
+                           <h3 className="head-desc text-center text-md-start">Mediacje</h3>
+                           <h3 className="head-desc text-center text-md-start">Negocjacje</h3>
+                           <h3 className="head-desc text-center text-md-start">Rozwiązywanie sporów</h3>
                        </div>
                    </div>
-                   <div className="col-md-5 col-lg-6 pt-3 pt-md-0 d-flex flex-column justify-content-center">
-                       <h2 className="display-4 head-name text-center text-md-start pb-1 pb-md-4">Izabela Kowalska</h2>
-                       <h3 className="head-desc text-center text-md-start">Mediacje</h3>
-                       <h3 className="head-desc text-center text-md-start">Negocjacje</h3>
-                       <h3 className="head-desc text-center text-md-start">Rozwiązywanie sporów</h3>
-                   </div>
                </div>
-           </div>
        </section>
-    // <section className="intro-section">
-    //     <div className="container">
-    //         <div className="row pt-2 pt-md-4 pt-lg-5">
-    //             <div className="col-md-7 col-lg-6">
-    //                 <div className="intro-img-container d-flex justify-content-center">
-    //                     <img src={photoBig} alt='zdjecie'
-    //                          className="img-fluid img-intro "/>
-    //                 </div>
-    //             </div>
-    //             <div className="col-md-5 col-lg-6 pt-3 pt-md-0 d-flex flex-column justify-content-center">
-    //                 <h2 className="display-4 intro-name text-center text-md-start pb-1 pb-md-4">Izabela Kowalska</h2>
-    //                 <h3 className="intro-desc text-center text-md-start">Mediacje</h3>
-    //                 <h3 className="intro-desc text-center text-md-start">Negocjacje</h3>
-    //                 <h3 className="intro-desc text-center text-md-start">Rozwiązywanie sporów</h3>
-    //             </div>
-    //         </div>
-    //     </div>
-    // </section>
     )
 };
 
