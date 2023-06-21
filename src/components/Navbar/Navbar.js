b94dddb07cd4d5e354f7759d94991224c5eacbf7
Kasia Półrolniczak <k.polrolniczak@gmail.com>
2023-06-21 12:11:40 +0200

    hamburger menu is closing after clicking, but the togglebutton is on x after clicking on link (should change to hamburger)

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -13,7 +13,10 @@ const Navbar = () => {
     setIsClicked(false)
   }
 
-  if (!isClicked) {
+  const styles = isClicked  ?
+      "navbar-container collapse navbar-collapse d-none d-lg-block d-lg-flex justify-content-end" :
+      "navbar-container collapse navbar-collapse d-lg-flex justify-content-end";
+
     return (
         <>
           <nav className="navbar bg-white navbar-expand-lg">
@@ -36,7 +39,7 @@ const Navbar = () => {
                 <span className="toggler-icon bottom-bar"></span>
               </button>
               <div
-                  className="navbar-container collapse navbar-collapse d-lg-flex justify-content-end"
+                  className={styles}
                   id="navbarNav">
                 <ul className="navbar-nav menu">
                   <li className="nav-item active">
@@ -74,132 +77,6 @@ const Navbar = () => {
           </nav>
         </>
         )
-  } else {
-    return (
-        <>
-          <nav className="navbar bg-white navbar-expand-lg">
-            <div className="container">
-              <Link to="/mediation-web" className="navbar-brand">
-                <img src={logo} role="button" className="logo"></img>
-              </Link>
-              <button
-                  className="navbar-toggler collapsed d-flex d-lg-none flex-column justify-content-around"
-                  type="button"
-                  data-bs-toggle="collapse"
-                  data-bs-target="#navbarNav"
-                  aria-controls="navbarNav"
-                  aria-expanded="false"
-                  aria-label="Toggle navigation"
-                  onClick={handleClickFalse}>
-                <span className="toggler-icon top-bar"></span>
-                <span className="toggler-icon middle-bar"></span>
-                <span className="toggler-icon bottom-bar"></span>
-              </button>
-              <div
-                  className="navbar-container collapse navbar-collapse d-none d-lg-block d-lg-flex justify-content-end"
-                  id="navbarNav">
-                <ul className="navbar-nav menu">
-                  <li className="nav-item active">
-                    <Link
-                        className="nav-link nav-link-color"
-                        aria-current="page"
-                        to="/mediation-web"
-                        onClick={handleClickTrue}>
-                      START
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/omnie" onClick={handleClickTrue}>
-                      O MNIE
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/omediacjach" onClick={handleClickTrue}>
-                      O MEDIACJACH
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/oferta" onClick={handleClickTrue}>
-                      OFERTA
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/kontakt" onClick={handleClickTrue}>
-                      KONTAKT
-                    </Link>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </nav>
-        </>
-    )
-  }
-
-
-
-
-  //
-  //
-  // return (
-  //   <>
-  //     <nav className="navbar bg-white navbar-expand-lg">
-  //       <div className="container">
-  //         <Link to="/mediation-web" className="navbar-brand">
-  //           <img src={logo} role="button" className="logo"></img>
-  //         </Link>
-  //         <button
-  //           className="navbar-toggler collapsed d-flex d-lg-none flex-column justify-content-around"
-  //           type="button"
-  //           data-bs-toggle="collapse"
-  //           data-bs-target="#navbarNav"
-  //           aria-controls="navbarNav"
-  //           aria-expanded="false"
-  //           aria-label="Toggle navigation"
-  //           onClick={handleClickFalse}>
-  //           <span className="toggler-icon top-bar"></span>
-  //           <span className="toggler-icon middle-bar"></span>
-  //           <span className="toggler-icon bottom-bar"></span>
-  //         </button>
-  //         <div
-  //           className="navbar-container collapse navbar-collapse d-lg-flex justify-content-end"
-  //           id="navbarNav">
-  //           <ul className="navbar-nav menu">
-  //             <li className="nav-item active">
-  //               <Link
-  //                 className="nav-link nav-link-color"
-  //                 aria-current="page"
-  //                 to="/mediation-web"
-  //                 onClick={handleClickFalse}>
-  //                 START
-  //               </Link>
-  //             </li>
-  //             <li className="nav-item">
-  //               <Link className="nav-link" to="/omnie" onClick={handleClickTrue}>
-  //                 O MNIE
-  //               </Link>
-  //             </li>
-  //             <li className="nav-item">
-  //               <Link className="nav-link" to="/omediacjach" onClick={handleClickTrue}>
-  //                 O MEDIACJACH
-  //               </Link>
-  //             </li>
-  //             <li className="nav-item">
-  //               <Link className="nav-link" to="/oferta" onClick={handleClickTrue}>
-  //                 OFERTA
-  //               </Link>
-  //             </li>
-  //             <li className="nav-item">
-  //               <Link className="nav-link" to="/kontakt" onClick={handleClickTrue}>
-  //                 KONTAKT
-  //               </Link>
-  //             </li>
-  //           </ul>
-  //         </div>
-  //       </div>
-  //     </nav>
-  //   </>
-  // );
 };
 
 export default Navbar;
