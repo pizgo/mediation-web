b61584b5a04dc70dd6261136f17d13310b337071
Kasia Półrolniczak <k.polrolniczak@gmail.com>
2023-06-21 14:37:29 +0200

    adjusting img-hero size, working on typography in hero section

--- src/components/LandingPage/Hero.js
+++ src/components/LandingPage/Hero.js
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <header className="hero-section">
         <div className="row pt-2 pt-md-4 pt-lg-5">
-          <div className="col-md-7 col-lg-6">
+          <div className="col-sm-6 ps-5">
             <div className="d-flex justify-content-center">
               <img
                 src={photoBig}
@@ -12,14 +12,13 @@ const Hero = () => {
                 className="img-fluid img-hero"/>
             </div>
           </div>
-          <div
-            className="col-md-5 col-lg-6 pt-3 pt-md-0 d-flex flex-column justify-content-center">
-            <h3 className="hero-name text-center text-md-start pb-1 pb-md-4">
+          <div className="col-sm-6 pt-3 pt-md-0 d-flex flex-column justify-content-center">
+            <h3 className="hero-name text-center text-sm-start pb-1 ">
               Izabela Kowalska
             </h3>
-            <p className="hero-desc text-center text-md-start">Mediacje</p>
-            <p className="hero-desc text-center text-md-start">Negocjacje</p>
-            <p className="hero-desc text-center text-md-start">Rozwiązywanie sporów</p>
+            <p className="hero-desc text-center text-sm-start">Mediacje</p>
+            <p className="hero-desc text-center text-sm-start">Negocjacje</p>
+            <p className="hero-desc text-center text-sm-start">Rozwiązywanie sporów</p>
           </div>
         </div>
     </header>
